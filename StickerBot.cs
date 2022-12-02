93e0defa71905734b131f962765dd235d76c860f
TwiWeYo <portportpor@gmail.com>
2022-12-02 22:11:10 +0300

    no editing, just the stickers (still doesn't work)

--- StickerBot.cs
+++ StickerBot.cs
@@ -2,6 +2,7 @@
 using LiteDB;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -16,8 +17,6 @@ namespace HarryPovarBot
     {
         private readonly ITelegramBotClient botClient;
         private readonly IRepository<BsonValue, Sticker> repository;
-        private StickerQuery? pendingSticker;
-        private long? editId;
         public StickerBot(IRepository<BsonValue, Sticker> repository)
         {
             if (repository is null)
@@ -35,7 +34,7 @@ namespace HarryPovarBot
             // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
             var receiverOptions = new ReceiverOptions
             {
-                AllowedUpdates = new[] { UpdateType.Message } // receive all update types
+                AllowedUpdates = new[] { UpdateType.Message }
             };
 
             botClient.StartReceiving(
@@ -58,59 +57,18 @@ namespace HarryPovarBot
         {
             if (update.Message is not { } message)
                 return;
+            if (message.Text is not { } messageText)
+                return;
 
             var chatId = message.Chat.Id;
-            if (pendingSticker is not null)
+
+            var result = repository.Get(
+                x => messageText.Contains(x.ReferenceString!, StringComparison.InvariantCultureIgnoreCase))
+                .FirstOrDefault();
+
+            if (result?.Id is not null)
             {
-                if (string.IsNullOrEmpty(pendingSticker.StickerId))
-                {
-                    if (message.Sticker is { } messageSticker)
-                    {
-                        pendingSticker.StickerId = messageSticker.FileId;
-                        await botClient.SendTextMessageAsync(chatId, "Пришлите ключевые слова для отправки стикера. Для завершения введите /finishSticker", cancellationToken: cancellationToken);
-                    }
-                    else
-                    {
-                        await botClient.SendTextMessageAsync(chatId, "Пришлите стикер", cancellationToken: cancellationToken);
-                    }
-                }
-                else if (message.Text is { } messageText)
-                {
-                    if (messageText.Split()[0] == "/finishSticker")
-                    {
-                        repository.Upsert(pendingSticker);
-                        pendingSticker = null;
-                    }
-                    else
-                    {
-                        pendingSticker.ReferenceStrings.Add(messageText);
-                    }
-                }
-            }
-            else if(message.Text is { } messageText1)
-            {
-                if (messageText1.Split()[0] == "/addSticker")
-                {
-                    if (pendingSticker is null)
-                    {
-                        pendingSticker = new StickerQuery();
-                        editId = chatId;
-                        await botClient.SendTextMessageAsync(chatId, "Пришлите стикер", cancellationToken: cancellationToken);
-                    }
-                    else
-                    {
-                        await botClient.SendTextMessageAsync(chatId, "Укажите параметры для стикера\nЧтобы завершить редактирование, введите /finishSticker",
-                            cancellationToken: cancellationToken);
-                    }
-                }
-                else
-                {
-                    var meme = repository.FindSticker(messageText1);
-                    if (meme?.StickerId is not null)
-                    {
-                        await botClient.SendStickerAsync(chatId, meme.StickerId, cancellationToken: cancellationToken);
-                    }
-                }
+                await botClient.SendStickerAsync(chatId, result.Id, cancellationToken: cancellationToken);
             }
         }
 
